b608e23e41630fabfcbb25b56ed48f5afd672c3c
Michael Rook <michael@rook.net.au>
2020-07-17 16:52:19 +1000

    Add config

--- src/ActionsAndFiltersServiceProvider.php
+++ src/ActionsAndFiltersServiceProvider.php
@@ -12,13 +12,15 @@ class ActionsAndFiltersServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('action', function ($expression) {
-            return "<?php Action::run({$expression}); ?>";
-        });
+        if(function_exists('config') && config('actions-filters.enable_blade')){
+            Blade::directive('action', function ($expression) {
+                return "<?php Action::run({$expression}); ?>";
+            });
 
-        Blade::directive('filter', function ($expression) {
-            return "<?php echo Filter::run({$expression}); ?>";
-        });
+            Blade::directive('filter', function ($expression) {
+                return "<?php echo Filter::run({$expression}); ?>";
+            });
+        }
     }
 
     /**
@@ -26,10 +28,15 @@ class ActionsAndFiltersServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'actions-filters');
+
+        // Register the Action class to use with the Action facade
         $this->app->singleton('action', function () {
             return new Action;
         });
 
+        // Register the Filter class to use with the Filter facade
         $this->app->singleton('filter', function () {
             return new Filter;
         });
