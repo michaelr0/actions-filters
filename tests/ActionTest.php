bdeb30df28a5e5a6cf251f6dc74c0f521acd7c52
Michael Rook <michael@rook.net.au>
2020-07-22 19:15:59 +1000

    Style CI Fixes

--- tests/ActionTest.php
+++ tests/ActionTest.php
@@ -2,9 +2,9 @@
 
 namespace Michaelr0\ActionsAndFilters\Tests;
 
-use Michaelr0\ActionsAndFilters\Action as ActionClass;
 use Orchestra\Testbench\TestCase;
 use Michaelr0\ActionsAndFilters\Facades\Action;
+use Michaelr0\ActionsAndFilters\Action as ActionClass;
 use Michaelr0\ActionsAndFilters\Providers\ActionsAndFiltersServiceProvider;
 
 class ActionTest extends TestCase
@@ -17,7 +17,8 @@ class ActionTest extends TestCase
     /** @test */
     public function actionsWork()
     {
-        $action = Action::add('test', function () {}, 10, 0);
+        $action = Action::add('test', function () {
+        }, 10, 0);
         $this->assertTrue($action instanceof ActionClass);
 
         Action::add('test', 'array_merge', 10, 2);
@@ -27,7 +28,7 @@ class ActionTest extends TestCase
 
         $this->assertTrue(Action::existsForCallback('test', 'array_merge'));
         $this->assertFalse(Action::existsForCallback('test', 'strtoupper'));
-        $this->assertFalse(Action::existsForCallback('test2','strtoupper'));
+        $this->assertFalse(Action::existsForCallback('test2', 'strtoupper'));
 
         $this->assertNotEmpty(Action::list('test'));
         $this->assertEmpty(Action::list('test2'));
@@ -48,10 +49,14 @@ class ActionTest extends TestCase
         $this->assertEmpty(Action::list('test'));
 
         $actionsWork = false;
-        Action::add('test', function () use (&$actionsWork) { $actionsWork = true; });
+        Action::add('test', function () use (&$actionsWork) {
+            $actionsWork = true;
+        });
         Action::run('test');
         $this->assertTrue($actionsWork);
-        Action::add('test', function () use (&$actionsWork) { $actionsWork = false; });
+        Action::add('test', function () use (&$actionsWork) {
+            $actionsWork = false;
+        });
         Action::run('test');
         $this->assertFalse($actionsWork);
 
@@ -63,7 +68,8 @@ class ActionTest extends TestCase
         Action::run('test', [], [], []);
 
         Action::removeAllFor('test');
-        Action::add('test', function () { return null; }, 10, 0);
+        Action::add('test', function () {
+        }, 10, 0);
         Action::run('test');
     }
 }
