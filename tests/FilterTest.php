9c28bab5b95e02dd69ef7da618543ed0686528ec
Michael Rook <michael@rook.net.au>
2020-07-22 19:26:06 +1000

    Refactor Filters Tests

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -4,6 +4,7 @@ namespace Michaelr0\ActionsAndFilters\Tests;
 
 use Orchestra\Testbench\TestCase;
 use Michaelr0\ActionsAndFilters\Facades\Filter;
+use Michaelr0\ActionsAndFilters\Filter as FilterClass;
 use Michaelr0\ActionsAndFilters\Providers\ActionsAndFiltersServiceProvider;
 
 class FilterTest extends TestCase
@@ -16,46 +17,47 @@ class FilterTest extends TestCase
     /** @test */
     public function filtersWork()
     {
-        // ** Add Filter and test if it worked ** //
-        Filter::add('Test', 'strtolower');
-        $this->assertTrue((Filter::run('Test', 'TEST') === 'test'));
-        // //** Add Filter and test if it worked ** //
+        $action = Filter::add('test', function () {
+        }, 10, 0);
+        $this->assertTrue($action instanceof FilterClass);
 
-        // ** Add Additional Filter and test if it worked ** //
-        Filter::add('Test', 'ucfirst');
-        $this->assertTrue((Filter::run('Test', 'TEST') === 'Test'));
-        // //** Add Additional Filter and test if it worked ** //
+        Filter::add('test', 'strtolower', 10, 2);
 
-        // ** Check Filters list and confirm it exists ** //
-        $this->assertTrue(is_array(Filter::list('Test')));
-        // //** Check Filters list and confirm it exists ** //
+        $this->assertTrue(Filter::exists('test'));
+        $this->assertFalse(Filter::exists('test2'));
 
-        // ** Remove Filters from list and test if it worked ** //
-        Filter::removeAllFor('Test');
+        $this->assertTrue(Filter::existsForCallback('test', 'strtolower'));
+        $this->assertFalse(Filter::existsForCallback('test', 'strtoupper'));
+        $this->assertFalse(Filter::existsForCallback('test2', 'strtoupper'));
 
-        $this->assertEmpty(Filter::list('Test'));
-        // //** Remove Filters from list and test if it worked ** //
+        $this->assertNotEmpty(Filter::list('test'));
+        $this->assertEmpty(Filter::list('test2'));
+        $this->assertEmpty(Filter::list('test', 9));
+        $this->assertNotEmpty(Filter::list('test', 10));
+        $this->assertEmpty(Filter::list('test2', 10));
 
-        Filter::add('Test', 'strtoupper');
-        $this->assertTrue(Filter::exists('Test'));
-        $this->assertTrue(Filter::existsForCallback('Test', 'strtoupper'));
-
-        $this->assertNotEmpty(Filter::list());
-        $this->assertNotEmpty(Filter::list('Test'));
-        $this->assertNotEmpty(Filter::list('Test', 10));
         $this->assertNotEmpty(Filter::listAll());
 
-        $this->assertFalse(Filter::exists('Should return false'));
-        $this->assertFalse(Filter::existsForCallback('Test', 'strtolower'));
-        $this->assertFalse(Filter::existsForCallback('Should return false', 'strtolower'));
+        Filter::remove('test', 'strtolower', 10, 3);
+        Filter::remove('test', 'strtoupper', 10, 2);
+        Filter::remove('test', 'strtolower', 9, 2);
+        $this->assertTrue(Filter::remove('test', 'strtolower', 10, 2) instanceof FilterClass);
+        Filter::remove('test', 'strtolower', 10, 2);
 
-        $this->assertEmpty(Filter::list('Should return empty'));
-        $this->assertEmpty(Filter::list('Should return empty', 10));
+        $this->assertTrue(Filter::removeAllFor('test') instanceof FilterClass);
 
-        $this->assertEmpty(Filter::list('Test', 9));
+        $this->assertEmpty(Filter::list('test'));
 
-        Filter::removeAllFor('Test');
+        Filter::removeAllFor('test');
 
-        $this->assertEmpty(Filter::list('Test'));
+        Filter::add('test', function() {
+            return "foobar";
+        }, 10, 0);
+        Filter::add('test', 'strtoupper', 10, 1);
+        Filter::add('test', function(...$args) {
+            return "{$args[0]} {$args[0]}";
+        }, 10, 2);
+
+        $this->assertTrue(Filter::run('test', null, null, null) === 'FOOBAR FOOBAR');
     }
 }
