0c62cb1af2b46f1106c06022cfdcb1be35c27263
Michael Rook <michael@rook.net.au>
2020-07-24 11:39:20 +1000

    Remove runRecursive functions.

--- src/Filter.php
+++ src/Filter.php
@@ -10,11 +10,27 @@ class Filter
 
     public function run(string $hook, ...$args)
     {
-        return $this->runRecursiveAndReturn($hook, $args);
-    }
+        $value = $args[0] ?? null;
 
-    public function runRecursive(string $hook, ...$args)
-    {
-        return $this->runRecursiveAndReturn($hook, $args);
+        if (! empty($this->listeners)) {
+            $argsCount = count($args);
+
+            foreach ($this->listeners[$hook] as $priority) {
+                foreach ($priority as $listener) {
+                    $args[0] = $value;
+
+                    if ($listener['arguments'] === 0) {
+                        $value = call_user_func($listener['callback']);
+                    } elseif ($listener['arguments'] >= $argsCount) {
+                        $value = call_user_func_array($listener['callback'], $args);
+                    } else {
+                        // Workaround if more args were passed than what the callback can accept
+                        $value = call_user_func_array($listener['callback'], array_slice($args, 0, $listener['arguments']));
+                    }
+                }
+            }
+        }
+
+        return $value;
     }
 }
